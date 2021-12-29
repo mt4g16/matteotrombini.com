ac670cee163eb17aca4e6ed89e048f7eacc192e2
Matteo <trombinimatteo@gmail.com>
2021-12-29 21:17:14 +0100

    Added: Fade-in and out for bio blurb and project buttons

--- scripts/anim.js
+++ scripts/anim.js
@@ -1,4 +1,7 @@
+
 var toggle = 0;
+var toggle2 = 1;
+
 
 function ani() {
     console.log(toggle);
@@ -17,6 +20,58 @@ function ani() {
         document.getElementById('tagline').classList.remove('animate_slideIn');
         document.getElementById('tagline').classList.add('animate_slideOut');
         toggle = 0;
+        console.log(toggle);
+        return
+
+    }
+
+}
+
+function fade() {
+
+
+    if (toggle2 == 0) {
+        document.getElementById('project_buttons').classList.remove('animate_fadeIn');
+        document.getElementById('project_buttons').classList.add('animate_fadeOut');
+        document.getElementById("project_buttons").style.opacity = 0;
+        toggle2 = 1;
+        console.log(toggle);
+        return
+
+    }
+
+    if (toggle2 == 1) {
+        document.getElementById('project_buttons').classList.remove('animate_fadeOut');
+        document.getElementById('project_buttons').classList.add('animate_fadeIn');
+        document.getElementById("project_buttons").style.opacity = 1;
+
+        toggle2 = 0;
+        console.log(toggle);
+        return
+
+    }
+
+}
+
+function fadeBlurb() {
+
+
+    if (toggle2 == 0) {
+        document.getElementById('project_buttons').classList.remove('animate_fadeIn');
+        document.getElementById('project_buttons').classList.add('animate_fadeOut');
+        document.getElementById("project_buttons").style.opacity = 0;
+        toggle2 = 1;
+        console.log(toggle);
+        return
+
+    }
+
+    if (toggle2 == 1) {
+        document.getElementById('project_buttons').classList.remove('animate_fadeOut');
+        document.getElementById('project_buttons').classList.add('animate_fadeIn');
+        document.getElementById("project_buttons").style.opacity = 1;
+
+        toggle2 = 0;
         console.log(toggle);
         return
 
